8aa722b8d7d9d3d4e6c59282f78ab97daebd8a92
xp-squared <zaneimportant@gmail.com>
2024-08-05 12:14:09 -0400

    Annotated and finished

--- Valid-Anagram.py
+++ Valid-Anagram.py
@@ -30,17 +30,54 @@ class Solution:
         # sorted function takes O(NlogN) time, to compare the elements when returning takes N amount of time
         # our time complexity is O(NlogN)
         return sorted(s) == sorted(t)
+    
+    def isAnagram2(self, s: str, t: str) -> bool:
+        '''
+        return Counter(s) == Counter(t)
+        
+        A Counter is a dict subclass for counting hashable objects. 
+        It is a collection where elements are stored as dictionary 
+        keys and their counts are stored as dictionary values. 
+        Counts are allowed to be any integer value including zero or 
+        negative counts.
+
+        We basically did this in the code below, same thing but just using the counter function instead
+        '''
+        if len(s) != len(t):
+            return False
+        # this time we are going to use a hash map
+        # time complexity is O(S + T), we have to iterate through both strings
+
+        countS, countT = {},{}
+        for i in range(len(s)):
+            # count each occurence in both strings
+            # s[i] is the key
+            # we want to increment the count of the characters we see
+            # we use the get function to get the key, if the key does not exist in the hashmap the default value returned is 0
+            countS[s[i]] = 1 + countS.get(s[i], 0)
+            countT[t[i]] = 1 + countT.get(t[i], 0)
+        return countS == countT
+        
 
 # Example usage:
 if __name__ == "__main__":
     solution = Solution()
-
+    print()
     s1 = "dog"
     t1 = "cat"
     s2 = "angel"
     t2 = "glean"
-
     result1 = solution.isAnagram1(s1, t1)
     print(f"Are '{s1}' and '{t1}' anagrams? {result1}")
     result2 = solution.isAnagram1(s2, t2)
     print(f"Are '{s2}' and '{t2}' anagrams? {result2}")
+
+    print()
+    s3 = "rag"
+    t3 = "bag"
+    s4 = "earth"
+    t4 = "heart"
+    result3 = solution.isAnagram2(s3, t3)
+    print(f"Are '{s3}' and '{t3}' anagrams? {result3}")
+    result4 = solution.isAnagram2(s4, t4)
+    print(f"Are '{s4}' and '{t4}' anagrams? {result4}")
