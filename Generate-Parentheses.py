1d127adf36d8b286b85faa910b8e3e0abf28686c
xp-squared <zaneimportant@gmail.com>
2024-08-13 11:29:19 -0400

    Starting

---
+++ Generate-Parentheses.py
@@ -0,0 +1,20 @@
+'''
+Generate Parentheses
+You are given an integer n. Return all well-formed parentheses strings that you can generate with n pairs of parentheses.
+
+Example 1:
+
+Input: n = 1
+
+Output: ["()"]
+Example 2:
+
+Input: n = 3
+
+Output: ["((()))","(()())","(())()","()(())","()()()"]
+You may return the answer in any order.
+
+Constraints:
+
+1 <= n <= 7
+'''
