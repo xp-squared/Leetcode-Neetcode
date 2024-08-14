6db111eb66fe93a8f207c57b72311c5dc8a54821
xp-squared <zaneimportant@gmail.com>
2024-08-14 12:13:47 -0400

    Finished

--- Valid-Palindrome.py
+++ Valid-Palindrome.py
@@ -22,6 +22,83 @@ Constraints:
 
 1 <= s.length <= 1000
 s is made up of only printable ASCII characters.
-
-
 '''
+
+class Solution:
+    def isPalindrome1(self, s: str) -> bool:
+        # two pointers section
+        # part of blind 75 questions
+        # we are ignoring upper and lower case
+        # first solution, convert all to lowercase and remove special characters
+        newStr = ""
+        for c in s:
+            if c.isalnum(): # if c is a-z and 0-9 we will go thru with it
+                newStr += c.lower()
+        return newStr == newStr[::-1] # syntax for reversing string
+    
+        # interviewer probably wont want us to use the functions so much to solve
+        # as well we used extra memory by making newStr
+        # good solution but not best for interviews
+
+    def isPalindrome2(self, s: str) -> bool:
+        # this is using 2 pointers, a left and a right pointer
+        # they move their respective directions until they either meet each other or pass each other
+        # to make our own alphanumeric function we are going to use ASCII values to make sure we only use letters and numbers
+        # memory complexity O(1), not using any extra memory
+
+        l = 0
+        r = len(s) - 1
+
+        while l < r:  # the pointers have not crossed each other 
+            while l < r and not self.alphaNum(s[l]):  # incrementing if not in our alphaNum function
+                l += 1
+            while r > l and not self.alphaNum(s[r]):
+                r -= 1
+            if s[l].lower() != s[r].lower():  # comparing characters of left and right and converting them to lowercase
+                return False
+            # after doing comparison want to update our left and right pointers
+            l = l + 1
+            r = r - 1
+
+        return True
+
+    def alphaNum(self, c):
+        # can get ASCII val by using ord function
+        # will only return something within the bounds given
+        return (ord('A') <= ord(c) <= ord('Z') or 
+                ord('a') <= ord(c) <= ord('z')  or 
+                ord('0') <= ord(c) <= ord('9'))
+
+
+
+if __name__ == "__main__":
+    solution = Solution()
+    
+    
+    test_str1 = "A man, a plan, a canal: Panama"
+    print(solution.isPalindrome1(test_str1))  # Output: True
+    
+    test_str2 = "race a car"
+    print(solution.isPalindrome1(test_str2))  # Output: False
+
+    test_str3 = " "
+    print(solution.isPalindrome1(test_str3))  # Output: True
+
+    
+    test_str4 = "Was it a car or a cat I saw?"
+    print(solution.isPalindrome2(test_str4))  # Output: True
+
+    test_str5 = "tab a cat"
+    print(solution.isPalindrome2(test_str5))  # Output: False
+
+    test_str6 = "No 'x' in Nixon"
+    print(solution.isPalindrome2(test_str6))  # Output: True
+
+    test_str7 = "Able was I, I saw Elba"
+    print(solution.isPalindrome2(test_str7))  # Output: True
+
+    test_str8 = "12321"
+    print(solution.isPalindrome2(test_str8))  # Output: True
+
+    test_str9 = "12345"
+    print(solution.isPalindrome2(test_str9))  # Output: False
