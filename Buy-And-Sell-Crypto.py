9ce70cceac12610dce666e85681cbecf4c3f56af
xp-squared <zaneimportant@gmail.com>
2024-08-26 15:09:25 -0400

    Finished

--- Buy-And-Sell-Crypto.py
+++ Buy-And-Sell-Crypto.py
@@ -26,3 +26,56 @@ Constraints:
 0 <= prices[i] <= 100
 
 '''
+
+from typing import List
+
+class Solution:
+    def maxProfit1(self, prices: List[int]) -> int:
+        maximumProf = 0
+        currentMax = 0
+        for i in range(len(prices) - 1):
+            for j in range(i + 1, len(prices)):
+                if prices[j] - prices[i] < 0:
+                    continue
+                else:
+                    currentMax = prices[j] - prices[i]
+                    maximumProf = max(maximumProf, currentMax)
+        return maximumProf
+    
+    # abduls answer
+    def maxProfit2(self, prices: List[int]) -> int:
+        res = 0
+        min_val = float("inf")
+        for price in prices:
+            min_val = min(min_val, price) 
+            res = max(res, price - min_val) 
+        return res 
+
+    def maxProfit3(self, prices: List[int]) -> int:
+        res = 0
+        lowest = prices[0]
+        for p in prices:
+            if p < lowest:
+                lowest = p
+            res = max(res, p - lowest)
+        return res
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    
+    prices1 = [7, 1, 5, 3, 6, 4]
+    print(solution.maxProfit1(prices1))  # Output: 5
+
+    prices2 = [7, 6, 4, 3, 1]
+    print(solution.maxProfit1(prices2))  # Output: 0
+
+    prices3 = [1, 2, 3, 4, 5]
+    print(solution.maxProfit1(prices3))  # Output: 4
+
+    prices4 = [2, 4, 1]
+    print(solution.maxProfit1(prices4))  # Output: 2
+
+    prices5 = [3, 2, 6, 5, 0, 3]
+    print(solution.maxProfit1(prices5))  # Output: 4
