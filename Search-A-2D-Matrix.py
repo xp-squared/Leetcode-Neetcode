b0af76eabf260ec22e32f605fab9a53c79c51ba7
xp-squared <zaneimportant@gmail.com>
2024-08-26 14:34:43 -0400

    Starting

--- Search-A-2D-Matrix.py
+++ Search-A-2D-Matrix.py
@@ -1,5 +1,10 @@
 '''
+Binary Search Problem
+Practice Counter: 2
+
 Search 2D Matrix
+
+
 You are given an m x n 2-D integer array matrix and an integer target.
 
 Each row in matrix is sorted in non-decreasing order.
@@ -14,7 +19,7 @@ Example 1:
 14 20 30 40
 
 Input: matrix = [[1,2,4,8],[10,11,12,13],[14,20,30,40]], target = 10
-Output: true
+Output: ue
 
 
 Example 2:
