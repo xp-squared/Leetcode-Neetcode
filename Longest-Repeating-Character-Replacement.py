642b6b22c4502d97849c894e7799e2caaedf2f88
xp-squared <zaneimportant@gmail.com>
2024-08-30 12:42:50 -0400

    Start

---
+++ Longest-Repeating-Character-Replacement.py
@@ -0,0 +1,26 @@
+'''
+Sliding Window Problem
+Practice Counter: 
+
+Longest Repeating Substring With Replacement
+You are given a string s consisting of only uppercase english characters and an integer k. You can choose up to k characters of the string and replace them with any other uppercase English character.
+
+After performing at most k replacements, return the length of the longest substring which contains only one distinct character.
+
+Example 1:
+
+Input: s = "XYYX", k = 2
+
+Output: 4
+Explanation: Either replace the 'X's with 'Y's, or replace the 'Y's with 'X's.
+
+Example 2:
+
+Input: s = "AAABABB", k = 1
+
+Output: 5
+Constraints:
+
+1 <= s.length <= 1000
+0 <= k <= s.length
+'''
