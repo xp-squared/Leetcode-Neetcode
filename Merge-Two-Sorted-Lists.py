beb169a493661a9c484e8ce61b02e3e77f508233
xp-squared <zaneimportant@gmail.com>
2024-08-30 12:57:53 -0400

    Restarting with different problem

---
+++ Merge-Two-Sorted-Lists.py
@@ -0,0 +1,30 @@
+'''
+LinkedList Problem
+Practice Counter:
+
+Merge Two Sorted Linked Lists
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+
+
+
+Input: list1 = [1,2,4], list2 = [1,3,5]
+
+Output: [1,1,2,3,4,5]
+Example 2:
+
+Input: list1 = [], list2 = [1,2]
+
+Output: [1,2]
+Example 3:
+
+Input: list1 = [], list2 = []
+
+Output: []
+
+'''
