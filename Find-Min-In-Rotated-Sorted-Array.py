573ed4f34df1820a82ba6c95d5f4a71dfb0dacac
xp-squared <zaneimportant@gmail.com>
2024-09-03 12:00:28 -0400

    Finished

--- Find-Min-In-Rotated-Sorted-Array.py
+++ Find-Min-In-Rotated-Sorted-Array.py
@@ -29,5 +29,50 @@ Constraints:
 
 1 <= nums.length <= 1000
 -1000 <= nums[i] <= 1000
-
 '''
+
+from typing import List
+
+class Solution:
+    def findMin(self, nums: List[int]) -> int:
+        # binary search problem but not traditional whatsoever
+        # example
+        # you have array nums = [1,2,3,4,5]
+        # it would be [3,4,5,1,2] if it was rotated 4 times.
+        # return minimum element of this array in O(log N) time
+
+        start = 0
+        end = len(nums) - 1
+        curr_min = float("inf") # make the current minimum the largest number possible so we can have our start minimum be as large as it is
+
+        while start < end:
+            middle = start + (end - start ) // 2 # accounts for going out of bounds with indexes
+            curr_min = min(curr_min, nums[middle]) # grab the current minimum
+            # if middle value is in left sorted portion of values and is greater we know we want to go to the right sorted portion of values as they will be smaller
+
+            # right has the min
+            # [3, 4, 5, 1, 2]
+            # mid (5) is > end (2)
+            # update start to mid + 1 as we know it will not be on the left side any more
+            if nums[middle] > nums[end]: # the middle of the list is more than the end of the list 
+                start = middle + 1
+            
+            # left has the min
+            # [3, 4, 1, 2]
+            # this would be the case that 1 is the middle, which would be our current min
+            # we would search to the left
+            # in this case though we already found our minimum
+            else:
+                end = middle - 1
+
+        return min(curr_min, nums[start])
+
+
+if __name__ == "__main__":
+    solution = Solution()
+
+    print(solution.findMin([3, 4, 5, 1, 2]))  # Output: 1
+    print(solution.findMin([4, 5, 6, 7, 0, 1, 2]))  # Output: 0
+    print(solution.findMin([11, 13, 15, 17]))  # Output: 11
+    print(solution.findMin([2, 1]))  # Output: 1
+    print(solution.findMin([1]))  # Output: 1
