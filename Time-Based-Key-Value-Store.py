0aa494f1e5399360983670263bfb4a52d7a9069b
xp-squared <zaneimportant@gmail.com>
2024-09-09 14:50:16 -0400

    Finished

--- Time-Based-Key-Value-Store.py
+++ Time-Based-Key-Value-Store.py
@@ -1,4 +1,6 @@
 '''
+This is a binary search problem
+
 Time Based Key-Value Store
 Implement a time-based key-value data structure that supports:
 
@@ -26,4 +28,61 @@ timeMap.get("alice", 1);           // return "happy"
 timeMap.get("alice", 2);           // return "happy", there is no value stored for timestamp 2, thus we return the value at timestamp 1.
 timeMap.set("alice", "sad", 3);    // store the key "alice" and value "sad" along with timestamp = 3.
 timeMap.get("alice", 3);           // return "sad"
+
+ALL THE TIMESTAMPS OF SET ARE STRICTLY INCREASING!!!
 '''
+
+class TimeMap:
+    # we are gonna have a key value and a list of values associated with the key
+    # that list is a pair of values
+    # Key : Values <val, timeStamp>
+    # "foo" : [["bar",1],["bar",2]]
+
+    # if you don't find exact match in key-value store you return the most recent one that is less than it
+    def __init__(self):
+        self.keyStore = {}  # key = string, value = list of [string, timestamp]
+        # initializing a dictionary in the constructor 
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keyStore:  # if the key is not in the map
+            self.keyStore[key] = []  # we want to insert the key into the map
+        self.keyStore[key].append([value, timestamp])  # if it does exist we just insert at key's location
+
+    def get(self, key: str, timestamp: int) -> str:
+        # Returns the most recent value of key if set was previously called on it
+        # based on most recent timestamp compared to one given, if user gives 3 and 1 was most recent we go to 1
+        # do this in binary search 
+        
+        res = ""  # if the key does not exist we return an empty string
+
+        # This line is accessing the keyStore dictionary and trying to retrieve
+        # the list of values associated with the given key. If the key is not 
+        # found in the dictionary, it returns an empty list [].
+        # foo : [["bar",1],["bar",2],["bar",3],["bar",4]]
+        values = self.keyStore.get(key, [])
+        l = 0
+        r = len(values) - 1
+        while l <= r:
+            m = (l + r) // 2
+            if values[m][1] <= timestamp:  # [1] is used to get that middle value's timestamp, if it is less than the one we are searching for, we move the left pointer up to get more accurate
+                res = values[m][0]  # this is a possible valid value, since the result has to be either the target or the most recent one less than the target 
+                l = m + 1
+            else:  # invalid to assign this to the result
+                # we do not need to assign something greater to the result; it is either equal or less
+                r = m - 1
+        return res
+
+
+# Example test cases
+if __name__ == "__main__":
+    timeMap = TimeMap()
+
+    # Test case 1
+    timeMap.set("foo", "bar", 1)  # store the key "foo" with value "bar" at timestamp 1
+    print(timeMap.get("foo", 1))  # return "bar"
+    print(timeMap.get("foo", 3))  # return "bar", because the closest time <= 3 is 1
+
+    # Test case 2
+    timeMap.set("foo", "bar2", 4)  # store the key "foo" with value "bar2" at timestamp 4
+    print(timeMap.get("foo", 4))  # return "bar2"
+    print(timeMap.get("foo", 5))  # return "bar2", because the closest time <= 5 is 4
