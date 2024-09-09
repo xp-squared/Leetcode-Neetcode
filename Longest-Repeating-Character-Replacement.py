f0ded96b2810e7fd315e86a60bf3a54fff0129fa
xp-squared <zaneimportant@gmail.com>
2024-09-09 15:38:43 -0400

    Finished

--- Longest-Repeating-Character-Replacement.py
+++ Longest-Repeating-Character-Replacement.py
@@ -22,3 +22,46 @@ Constraints:
 0 <= k <= s.length
 
 '''
+
+class Solution:
+    def characterReplacement(self, s: str, k: int) -> int:
+        # sliding window problem
+
+        # you are given a string like XYYX
+        # as well a integer k
+        # you can shoose any chracter of the string and change it to any other UPPERCASE character
+        # want to create the longest possible substring with the same letter
+        # changing XYYX to XXXX would give us a result of 4
+        # This takes O(N * 26) time
+
+        count = {} # hash map to count occurence of each character
+        # if the current window length - count of most frequent letter 
+        # is less than or equal to k means our current window is valid
+        # if not we move our left pointer
+        res = 0
+
+        l = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0) # for string[r] we are adding to its count in the hashmap, if it is not in there it is automatically 0
+            
+            while (r - l + 1) - max(count.values()) > k: 
+                # to explain the above line, if your current window length - most frequent letter is greater than k we have to move our window
+                count[s[l]] -= 1 # decrementing the character at our left most position in the hash map so we have valid counts
+                l += 1
+            res = max(res, r - l + 1) # result from previous results and current size of the window + 1
+        return res
+
+
+# Example test cases
+if __name__ == "__main__":
+    solution = Solution()
+
+    print(solution.characterReplacement("AABABBA", 1))  # Output: 4
+
+    print(solution.characterReplacement("ABAB", 2))  # Output: 4
+
+    print(solution.characterReplacement("AAAA", 2))  # Output: 4
+
+    print(solution.characterReplacement("ABDDFFG", 3))  # Output: 5
+
